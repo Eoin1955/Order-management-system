7aa6de82ac27e3d7a0e3045800027763a9280faa
eoin <g00431577@atu.ie>
2025-01-09 16:49:54 +0000

    comment

--- src/main/java/ie/atu/test/Order.java
+++ src/main/java/ie/atu/test/Order.java
@@ -11,7 +11,7 @@ public class Order {//help me
         this.name = "";
         this.product_name = "";
         this.quantity = Integer.parseInt("");
-    }
+    }//liki
 
     public void Order(){
         this.orderid = orderid;
